64b3a1f80c97b2b7ab8ecbe16786e64af0dd532b
thedigitalmenagerie <honeyraeswan@gmail.com>
2021-08-12 10:51:42 -0400

    finished

---
+++ TheFamilyDictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TheFamilyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+            myFamily.Add("sister", new Dictionary<string, string>() { { "name", "Krista" }, { "age", "42" } });
+
+            foreach (var member in myFamily)
+            {
+                Console.WriteLine($"{member.Value["name"]} is my {member.Key} and is {member.Value["age"]} years old.");
+            }
+        }
+    }
+}
